7ca7e8a78bd4ca23a0ca489f179a83c1cb9a152c
Hunter <78630212+Hunter-614@users.noreply.github.com>
2025-04-15 08:58:44 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Simple menu button interaction
-const buttons = document.querySelectorAll('.menu-button');
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    buttons.forEach(b => b.classList.remove('selected'));
-    button.classList.add('selected');
-  });
-});
