abadd694f949cf243ce65d06c9559635852e3423
Hunter <78630212+Hunter-614@users.noreply.github.com>
2025-04-14 23:59:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function hoverSound() {
+  const audio = document.getElementById('hoverSound');
+  audio.currentTime = 0;
+  audio.play();
+}
+function clickSound() {
+  const audio = document.getElementById('clickSound');
+  audio.currentTime = 0;
+  audio.play();
+}
